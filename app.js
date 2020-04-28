0130dba30e58f47a2d8e70d6f11b5175eb69ecc6
Amber <amber.waltens@hotmail.com>
2020-04-28 15:20:04 +0200

    typo fixed

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-const apiimddollarRouter = require('./routes/api/v1/index');
+const apiImdollarRouter = require('./routes/api/v1/index');
 
 const app = express();
 
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/api/v1', apiimddollarRouter);
+app.use('/api/v1', apiImdollarRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
