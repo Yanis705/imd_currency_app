c4da68e9a0916b0cabfaece3060bcbd034f95095
Amber <amber.waltens@hotmail.com>
2020-05-07 11:21:31 +0200

    added passport(-local-mongoose) and User model basic

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+const authController = require('./../controllers/auth')
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.send('respond with a resource');
 });
+
+router.post('/signup', (req, res, next)=>{
+  //signup
+  //username uit req
+  //password uit req
+  //brypt encrypt
+  //databank
+})
 
 module.exports = router;
