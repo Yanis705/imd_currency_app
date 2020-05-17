61224a3a911c8be8bbe6875e66879942efb15e9e
Yanis De Maesschalck <yanis705@icloud.com>
2020-05-18 00:17:37 +0200

    Transfer money from one user to another

--- controllers/api/v1/transfers.js
+++ controllers/api/v1/transfers.js
@@ -1,4 +1,5 @@
 const Transfer = require('../../../models/Transfer');
+const User = require('../../../models/User');
 
 const getAll = (req, res) => {
     Transfer.find({}, (err, docs)=>{
@@ -21,29 +22,56 @@ const getAll = (req, res) => {
 
 const create = (req, res) => {
     let transfer = new Transfer()
-    transfer.from = req.body.from
+    transfer.from = req.user.username
     transfer.to = req.body.to
     transfer.IMDollars = req.body.IMDollars
     transfer.date = new Date().toLocaleDateString()
     transfer.reason = req.body.reason
     transfer.message = req.body.message
-    transfer.save((err, doc) =>{
-        if(err){
-            res.json({
-                "status": "error",
-                "message": "could not create this transfer"
-            })
-        }
 
-        if(!err){
-            res.json({
-                "status": "success",
-                "data": {
-                    "transfer": doc
+    User.find({username: req.user.username}, (err, sender) =>{
+        if(sender[0].balance >= transfer.IMDollars){
+            console.log("Balance OK!");
+            User.find({username: transfer.to}, (err, receiver) =>{
+                if(receiver[0] != null){
+                    console.log("User found");
+                    let actualBalanceSender = sender[0].balance;
+                    let newBalanceSender = actualBalanceSender - transfer.IMDollars;
+                    let balanceReceiver = receiver[0].balance;
+                    let newBalanceReceiver = balanceReceiver + transfer.IMDollars;
+                    User.findOneAndUpdate({username: req.user.username}, {balance: newBalanceSender}, (err, doc) => {
+                    });
+                    User.findOneAndUpdate({username: transfer.to}, {balance: newBalanceReceiver}, (err, doc) => {
+                    });
+                } else {
+                    console.log("User not found");
                 }
-            })
+                if(err){
+                    console.log(err);
+                }
+            });
+        } else {
+            console.log("Insufficient balance!");
         }
-    })
+    });
+
+    // transfer.save((err, doc) =>{
+    //     if(err){
+    //         res.json({
+    //             "status": "error",
+    //             "message": "could not create this transfer"
+    //         })
+    //     }
+
+    //     if(!err){
+    //         res.json({
+    //             "status": "success",
+    //             "data": {
+    //                 "transfer": doc
+    //             }
+    //         })
+    //     }
+    // })
     
 }
 
