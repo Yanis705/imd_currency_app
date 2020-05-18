94462b1cdcf3814b25f8775c6521b377d7f4600f
Amber <amber.waltens@hotmail.com>
2020-05-18 13:50:11 +0200

    added gulp -> imageConv

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,21 @@
 const {src, dest, watch} = require('gulp');
 const sass = require('gulp-sass');
 sass.compiler = require('node-sass');
+const image = require('gulp-image');
 
 sass2css = function(){
     return src("./public/stylesheets/style.scss")
         .pipe(sass().on("error", sass.logError))
         .pipe(dest("./public/stylesheets/dist/"))    
 };
-
+ 
+imageConv = function () {
+  return src('./public/images/*')
+    .pipe(image())
+    .pipe(dest('./public/convImages/'));
+};
+ 
 exports.default = function(){
     watch("./public/stylesheets/**/*.scss", sass2css);
+    watch("./public/images/**", imageConv);
 }
