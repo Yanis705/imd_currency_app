e811abd1414132d7374cefbc1a36bdab40cbb2b8
Yanis De Maesschalck <yanis705@icloud.com>
2020-05-19 19:04:10 +0200

    Added error handeling on new transfer

--- public/javascript/transfer.js
+++ public/javascript/transfer.js
@@ -1,28 +1,49 @@
-document.querySelector("#newTransaction").addEventListener('click', (e)=>{
+document.querySelector("#newTransaction").addEventListener('click', (e) => {
+
+    let transfer = true;
+
+    document.querySelector('.error.general').innerHTML = "";
+    document.querySelector('.error.to').innerHTML = "";
+    document.querySelector('.error.amount').innerHTML = "";
+
     let to = document.querySelector('#transactionTo').value;
     let IMDollars = document.querySelector('#amount').value;
     let reason = document.querySelector('#reason').value;
     let message = document.querySelector('#message').value;
 
-    fetch("http://localhost:3000/api/v1/transfers", {
-        method: "POST",
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + localStorage.getItem('token')
-        },
-        body: JSON.stringify({
-            "to": to,
-            "IMDollars": IMDollars,
-            "reason": reason,
-            "message": message,
+    if (to == "") {
+        document.querySelector('.error.to').innerHTML = "This field cannot be empty";
+        transfer = false;
+    }
+
+    if (IMDollars <= 0) {
+        document.querySelector('.error.amount').innerHTML = "You have to send at least 1 IMDollar";
+        transfer = false;
+    }
+
+    if (transfer) {
+        fetch("http://localhost:3000/api/v1/transfers", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + localStorage.getItem('token')
+            },
+            body: JSON.stringify({
+                "to": to,
+                "IMDollars": IMDollars,
+                "reason": reason,
+                "message": message,
+            })
+        }).then(response => {
+            return response.json();
+        }).then(json => {
+            if (json.status === "success") {
+                window.location = '/transactionCompleted';
+            } else if (json.status === "error") {
+                document.querySelector('.error.general').innerHTML = json.message;
+            }
         })
-    }).then(response => {
-        return response.json();
-    }).then(json => {
-        if (json.status === "success") {
-            window.location = '/transactionCompleted';
-        }
-    })
+    }
 
     e.preventDefault();
 })
